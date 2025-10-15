5ac97cde8fde2068106fcb21f97c1f4954344fc7
RAMgo008 <ramgoyal2005@gmail.com>
2025-10-15 19:34:51 +0530

    try

---
+++ Linked_List(Medium)/2.ReverseLL(iterative).cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if((head==nullptr)||(head->next==nullptr))
+        return head;
+        ListNode *last=reverseList(head->next);
+        head->next->next=head;
+        head->next=nullptr;
+        return last;
+    }
+};
+
+// Utility function to create linked list
+ListNode* createList(const vector<int>& vals) {
+    if (vals.empty()) return nullptr;
+    ListNode* head = new ListNode(vals[0]);
+    ListNode* curr = head;
+    for (int i = 1; i < vals.size(); i++) {
+        curr->next = new ListNode(vals[i]);
+        curr = curr->next;
+    }
+    return head;
+}
+
+// Utility function to print linked list
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> vals = {1, 2, 3, 4, 5};
+    ListNode* head = createList(vals);
+
+    cout << "Original list: ";
+    printList(head);
+
+    Solution sol;
+    ListNode* reversed = sol.reverseList(head);
+
+    cout << "Reversed list: ";
+    printList(reversed);
+
+    return 0;
+}
